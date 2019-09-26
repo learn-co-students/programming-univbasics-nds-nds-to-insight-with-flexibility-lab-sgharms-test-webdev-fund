42954025aa263e6d9ba39bcf2d492160d55e0958
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-26 14:11:03 -0400

    Clarify movies_with_directors_set test

--- spec/nds_extract_spec.rb
+++ spec/nds_extract_spec.rb
@@ -34,23 +34,40 @@ describe 'movies_with_director_key' do
 end
 
 describe 'movies_with_directors_set' do
-  it 'creates AoA of Hashes describing movies and each movie has a :director key' do
-    # { :name => "A", :movies => [{ :title => "Test" }] }
-    # becomes... [[{:title => "Test", :director => "A"}]]
-    test_data = [
-      { :name => "Byron Poodle", :movies => [
-        { :title => "At the park" },
-        { :title => "On the couch" },
+  describe 'creates AoA of Hashes describing movies and each movie has a :director key' do
+    it 'correctly sets Byron Poodle as director of the first film' do
+      # { :name => "A", :movies => [{ :title => "Test" }] }
+      # becomes... [[{:title => "Test", :director_name => "A"}]]
+      test_data = [
+        { :name => "Byron Poodle", :movies => [
+          { :title => "At the park" },
+          { :title => "On the couch" },
+        ]
+        },
+        { :name => "Nancy Drew", :movies => [
+          { :title => "Biting" },
+        ]
+        }
       ]
-      },
-      { :name => "Nancy Drew", :movies => [
-        { :title => "Biting" },
+      results = movies_with_directors_set(test_data)
+      expect(results.first.first[:director_name]).to eq("Byron Poodle")
+    end
+    it 'correctly sets Nancy Drew as director of the last film' do
+      test_data = [
+        { :name => "Byron Poodle", :movies => [
+          { :title => "At the park" },
+          { :title => "On the couch" },
+        ]
+        },
+        { :name => "Nancy Drew", :movies => [
+          { :title => "Biting" },
+        ]
+        }
       ]
-      }
-    ]
-    results = movies_with_directors_set(test_data)
-    expect(results.first.first[:director]).to eq("Byron Poodle")
-    expect(results.first.first[:director]).to eq("Nancy Drew")
+      results = movies_with_directors_set(test_data)
+
+      expect(results.last.first[:director_name]).to eq("Nancy Drew")
+    end
   end
 end
 
