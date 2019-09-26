a670db4f66132fc15b3303bf3e83f6f87b57b7ec
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-26 14:13:02 -0400

    Solution

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,4 @@
 # Provided, don't edit
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
@@ -49,12 +48,22 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+
+  result = []
+  i = 0
+
+  while i < movies_collection.length do
+    movie_data = movies_collection[i]
+    result << movie_with_director_name(name, movie_data)
+    i += 1
+  end
+
+  result
 end
 
 
 def gross_per_studio(collection)
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # create a 
+  # GOAL: Transform NDS'
   #
   # INPUT:
   #
@@ -64,6 +73,21 @@ def gross_per_studio(collection)
   #
   # * Hash whose keys are the studio names and whose values are the sum
   #   total of all the worldwide_gross numbers for every movie in the input Hash
+  result = {}
+  i = 0
+
+  while i < collection.length do
+    movie = collection[i]
+
+    if !result[movie[:studio]]
+      result[movie[:studio]] = movie[:worldwide_gross]
+    else
+      result[movie[:studio]] += movie[:worldwide_gross]
+    end
+    i += 1
+  end
+
+  result
 end
 
 def movies_with_directors_set(source)
@@ -78,6 +102,19 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it. You should use the provided
   # do that work in the movies_with_director_key method
+
+  i = 0
+  a_o_a_movies_by_dir = []
+
+  while i < source.length do
+    dir_info_hash = source[i]
+    director_name = dir_info_hash[:name]
+    directors_movies = dir_info_hash[:movies]
+    a_o_a_movies_by_dir << movies_with_director_key(director_name, directors_movies)
+    i += 1
+  end
+
+  a_o_a_movies_by_dir
 end
 
 # ----------------    End of Your Code Region --------------------
